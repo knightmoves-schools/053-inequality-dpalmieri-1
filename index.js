cfbf4754f19e08983c7788670114afae6be03aa4
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-16 13:07:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function guessNumber(guess){
     if (guess != 25){
-        document.getElementById('result').innerHTML = 'not it';
+        return 'not it';
     }
 }
 function strictGuessNumber(guess){
     if (guess !== 25){
-        document.getElementById('result').innerHTML += 'not it';
+        return 'not it';
     }
 }
+
 
